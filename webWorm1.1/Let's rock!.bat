baea3ace31ee20b8e99269e1e54a8fda97682491
Vance Yang <yzl5588@gmail.com>
2017-06-03 11:17:24 +0800

    V1.1
    1.1版本

---
+++ webWorm1.1/Let's rock!.bat
@@ -0,0 +1,1 @@
+py webWorm.py
---
+++ webWorm1.1/webWorm.py
@@ -0,0 +1,33 @@
+﻿import urllib.request
+import requests
+import os
+urlForwd = input('请输入搜索网站日期以前的信息（复制进来即可）：')
+urlAft = input('请输入搜索网站日期以后的信息：')
+yearlow = int(input('请输入搜索起始年份：'))
+yearup = int(input('请输入搜索结束年份：'))
+ifDown = input('如果搜索到文件，是否下载到本地？Y/N：')
+if ifDown == 'Y'or ifDown == 'y':
+    adress1 = input('请问你要创建在那个盘？：')+':/'
+    address = adress1+input('请输入你要创建文件夹名称：')
+    if not os.path.exists(address):
+        os.mkdir(address)
+availableData = [] #可用的日期集合
+for m in range(yearlow,yearup+1):        #年循环
+    for j in range(1,13):                #月循环
+        for i in range(1,32):            #日循环
+            date = str(m).zfill(2)+str(j).zfill(2)+str(i).zfill(2)
+            url = urlForwd+date+urlAft
+            try:
+                urllib.request.urlopen(url)
+                availableData.append(url)
+                if ifDown == 'Y' or ifDown == 'y':
+                    r = requests.get(url)
+                    with open(address+'/'+date+'Report'+'.pdf','wb') as f:
+                        f.write(r.content)
+                        print(date+'这个日期的文件已成功下载')
+            except urllib.error.HTTPError:
+                print(date+'这个日期没有内容')
+            except urllib.error.URLError:
+                print('这个网页有问题')
+print(availableData)
+
